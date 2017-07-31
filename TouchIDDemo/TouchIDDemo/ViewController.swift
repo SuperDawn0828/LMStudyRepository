604b49d31372f5d218d50deb2e8649c96686161e
黎明 <gdk216@yeah.net>
2017-07-31 15:39:26 +0800

    [添加]添加TouchID demo

---
+++ TouchIDDemo/TouchIDDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchIDDemo
+//
+//  Created by 黎明 on 2017/7/31.
+//  Copyright © 2017年 黎明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("=======")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
