af641321b53e13d2493c82c2bd7ea78b318c74db
黎明 <gdk216@yeah.net>
2018-01-03 09:55:00 +0800

    no message

---
+++ DatabaseTest/DatabaseTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatabaseTest
+//
+//  Created by 黎明 on 2017/10/22.
+//  Copyright © 2017年 黎明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
